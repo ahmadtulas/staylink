0682ab04eb52f14661f101f00c35142bf6ab0764
unknown <aizaz@theglocaluniversity.in>
2024-01-16 14:59:13 +0530

    few Migration Created

---
+++ database/migrations/2024_01_16_090219_create_rooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('propertyId')->length(1000);
+            $table->bigInteger('catId')->length(1000);
+            $table->string("name");
+            $table->bigInteger('price')->length(1000);
+            $table->bigInteger('capacity')->length(1000);
+            $table->bigInteger('isActive')->length(11);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
